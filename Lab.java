3e8dc8579f004c35979792014e38deab30900ad6
WilliamEhlers <william.ehlers@student.fairfield.edu>
2024-09-18 09:15:03 -0400

    Update Lab.java

--- Lab.java
+++ Lab.java
@@ -2,6 +2,8 @@ import java.io.*;
 public class Lab {
     public static void main(String[] args) {
         File file = new File("wolf.txt");
+        File file2 = new File("dino.txt");
+        File file3 = new File("orcas.txt");
         
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             String st;
