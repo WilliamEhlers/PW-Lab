2e16b6294c7e00dbb800418959e01cb918791516
WilliamEhlers <william.ehlers@student.fairfield.edu>
2024-09-18 08:53:36 -0400

    Update Lab.java

--- Lab.java
+++ Lab.java
@@ -1,1 +1,17 @@
-Public class Lab;
+import java.io.*;
+public class Lab {
+    public static void main(String[] args) {
+        File file = new File("C:\\Users\\billy\\OneDrive\\Documents\\HaolinLab\\tester.txt");
+        
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String st;
+            while ((st = br.readLine()) != null) {
+                System.out.println(st);
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found: " + e.getMessage());
+        } catch (IOException e) {
+            System.err.println("IOException: " + e.getMessage());
+        }
+    }
+}
