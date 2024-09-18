d039d4751ae6f9439ce5ab264eaace970109cebe
WilliamEhlers <william.ehlers@student.fairfield.edu>
2024-09-18 08:54:03 -0400

    Update Lab.java

--- Lab.java
+++ Lab.java
@@ -1,7 +1,7 @@
 import java.io.*;
 public class Lab {
     public static void main(String[] args) {
-        File file = new File("C:\\Users\\billy\\OneDrive\\Documents\\HaolinLab\\tester.txt");
+        File file = new File("wolf.txt");
         
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             String st;
