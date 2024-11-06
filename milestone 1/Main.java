3a6ab331af85bec14081a32429c0d0801a52e470
WilliamEhlers <william.ehlers@student.fairfield.edu>
2024-11-06 08:28:28 -0500

    Update Main.java

--- milestone 1/Main.java
+++ milestone 1/Main.java
@@ -7,16 +7,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        // Path to the directory with text files and stop words
         String directoryPath = "C:\\Users\\billy\\OneDrive\\Documents\\PW-Lab-main\\main\\files project";
         String stopWordsFilePath = "C:\\Users\\billy\\OneDrive\\Documents\\PW-Lab-main\\main\\stopwords.txt";
+        String positiveWordsFilePath = "C:\\Users\\billy\\OneDrive\\Documents\\PW-Lab-main\\main\\positivewords.txt";
+        String negativeWordsFilePath = "C:\\Users\\billy\\OneDrive\\Documents\\PW-Lab-main\\main\\negativewords.txt";
+        
         File directory = new File(directoryPath);
 
-        // Checks directory
         if (directory.isDirectory()) {
-            // Show all the files
             File[] files = directory.listFiles();
-            if (files != null && files.length > 0) {  // check if there are files
+            if (files != null && files.length > 0) {  
                 System.out.println("Which file do you want to read?");
                 for (int i = 0; i < files.length; i++) {
                     if (files[i].isFile()) {
@@ -32,11 +32,18 @@ public class Main {
                     File selectedFile = files[choice - 1];
                     System.out.println("You have selected: " + selectedFile.getName());
 
-                    TextProcessor processor = new TextProcessor(stopWordsFilePath);
+                    TextProcessor processor = new TextProcessor(stopWordsFilePath, positiveWordsFilePath, negativeWordsFilePath);
+                    
                     List<String> filteredWords = processor.processFileForUnique(selectedFile);
                     Statistics stats = new Statistics(filteredWords);
-                stats.displayStatistics();
+                    stats.displayStatistics();
 
+                    var toneCounts = processor.analyzeTone(filteredWords);
+                    System.out.println("Positive words count: " + toneCounts.get("positive"));
+                    System.out.println("Negative words count: " + toneCounts.get("negative"));
+                    
+                    String tone = toneCounts.get("positive") > toneCounts.get("negative") ? "Positive" : "Negative";
+                    System.out.println("Overall tone for " + selectedFile.getName() + ": " + tone);
 
                 } else {
                     System.out.println("Invalid selection.");
@@ -52,4 +59,3 @@ public class Main {
         }
     }
 }
-
